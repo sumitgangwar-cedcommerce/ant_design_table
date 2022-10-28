c98ca77ebb82a79a8f4a35bae83a178776a57b15
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-10-28 11:23:42 +0530

    antDesign Table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { Table } from "antd";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Action",
+      dataIndex: "",
+      render: (record) => <a href="#" onClick={()=>myFun(record)}>Delete</a>
+    },
+  ];
+  const [data , setData] = useState([
+    {
+      key: 1,
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      description:
+        "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
+    },
+    {
+      key: 2,
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      description:
+        "My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.",
+    },
+    {
+      key: 3,
+      name: "Not Expandable",
+      age: 29,
+      address: "Jiangsu No. 1 Lake Park",
+      description: "This not expandable",
+    },
+    {
+      key: 4,
+      name: "Joe Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+      description:
+        "My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.",
+    },
+  ]);
+
+  const myFun = (data) =>{
+    setData(prev => prev.filter(item => item!==data))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table
+      columns={columns}
+      dataSource={data}
+    />
   );
-}
-
+};
 export default App;
